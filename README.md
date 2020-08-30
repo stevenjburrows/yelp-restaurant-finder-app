592fb350cae9e27a080897e8662a166f196fac90
steven <steven@burrows.plus.com>
2020-08-30 16:00:33 +0100

    added a readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Yelp restaurant finder
+
+This is a project using the Yelp API to find restaurants then split the listing up based on their prices.
+
+## Looks
+
+The app will display an image from the with the ratings below the image. The list will scroll vertically with the price range going horizontal.
+
+## Details
+
+when you click on the restaurant it will show you more images from that restaurants listing in Yelp.
---
+++ src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const SearchBar = ({ term, onTermChange, onTernSubmit }) => {
+  return (
+    <View style={styles.backgroundStye}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        placeholder="Search"
+        style={styles.inputStyle}
+        value={term}
+        onChangeText={onTermChange}
+        autoCapitalize="none"
+        autoCorrect={false}
+        onEndEditing={onTernSubmit}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStye: {
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 10,
+    marginHorizontal: 15,
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
+  },
+});
+
+export default SearchBar;
